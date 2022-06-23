7e4efea779434662c38e12cc0f9664107126df83
Sovrano28 <Adeyemiayokanmi47@gmail.com>
2022-06-23 20:36:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Intro-to-Javascript-1
+ An intro to Javascript - a week 5 task from Zuriteam
---
+++ life.js
@@ -0,0 +1,3 @@
+document.getElementById("name").innerHTML = 'Name: Adeyemi Ayokanmi Joshua';
+document.getElementById("height").innerHTML = 'Height: 6.7 feet';
+document.getElementById("country").innerHTML = 'Country: Nigeria';
